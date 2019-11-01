013224e596c10218821cf60902a08540cefc408b
Mihir <mihirdm20@gmail.com>
2019-10-31 20:41:34 -0400

    Add existing file

---
+++ src/test/java/com/jenpipjunit/examples/TestMagicBuilder.java
@@ -0,0 +1,14 @@
+package com.jenpipjunit.examples;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestMagicBuilder {
+
+    @Test
+    public void testLucky() {
+        assertEquals(7, MagicBuilder.getLucky());
+    }
+
+}
---
+++ src/test/java/com/jenpipjunit/examples/TestMessageBuilder.java
@@ -0,0 +1,19 @@
+package com.jenpipjunit.examples;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestMessageBuilder {
+
+    @Test
+    public void testHelloWorld() {
+        assertEquals("hello world", MessageBuilder.getHelloWorld());
+    }
+
+    @Test
+    public void testNumber10() {
+        assertEquals(10, MessageBuilder.getNumber10());
+    }
+
+}
